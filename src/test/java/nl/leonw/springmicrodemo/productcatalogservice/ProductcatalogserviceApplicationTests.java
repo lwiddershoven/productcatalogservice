27a81fc246b3705b53af2dde4bd35eb57069b80c
Leon <qaz@dds.nl>
2020-12-01 11:18:49 +0100

    From start.spring.io

---
+++ src/test/java/nl/leonw/springmicrodemo/productcatalogservice/ProductcatalogserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.leonw.springmicrodemo.productcatalogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductcatalogserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
